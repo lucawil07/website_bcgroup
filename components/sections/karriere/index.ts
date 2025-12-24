c90d287be30c0b2df3f3093d526e7e6aef003fb8
lucawil07 <154284273+lucawil07@users.noreply.github.com>
2025-12-24 03:25:58 +0100

    Refactor code structure for improved readability and maintainability

--- components/sections/karriere/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as KarriereHero } from './KarriereHero'
-export { default as WhyJoinBCGroup } from './WhyJoinBCGroup'
-export { default as OpenPositions } from './OpenPositions'
-export { default as ApplicationProcess } from './ApplicationProcess'
-export { default as CompanyCulture } from './CompanyCulture'
-export { default as KarriereCTA } from './KarriereCTA'
-export { default as KarriereContactCTA } from './KarriereContactCTA'
